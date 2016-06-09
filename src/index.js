52766cca427ad72fe06110999281178a5bd06a90
Joon Ho Cho <joonho1101@gmail.com>
2016-06-09 06:11:08 -0700

    v0.0.4 parseValue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import {GraphQLScalarType} from 'graphql';
 import {GraphQLError} from 'graphql/error';
 import {Kind} from 'graphql/language';
 
-const coerceString = (value) => value;
+
+function isString(value) {
+  return typeof value === 'string';
+}
+
+function coerceString(value) {
+  if (isString(value)) {
+    return String(value);
+  }
+  return null;
+}
 
 export default ({
   typeName,
@@ -31,59 +41,70 @@ export default ({
   }
 
   const error = (value, ast, message) => {
-    throw new GraphQLError(`Argument "${argName}" has invalid value ${JSON.stringify(value)}.${message ? ` ${message}` : ''}.`, [ast]);
+    throw new GraphQLError(`Argument "${argName}" has invalid value ${JSON.stringify(value)}.${message ? ` ${message}` : ''}.`, ast ? [ast] : []);
+  };
+
+  const parseValue = (value, ast) => {
+    value = coerceString(value);
+    if (value == null) {
+      return null;
+    }
+
+    if (trim) {
+      value = value.trim();
+    } else {
+      if (trimLeft) {
+        value = value.trimLeft();
+      }
+      if (trimRight) {
+        value = value.trimRight();
+      }
+    }
+
+    if (truncate != null) {
+      value = value.substring(0, truncate);
+    }
+
+    if (transform) {
+      value = transform(value);
+      if (!isString(value)) {
+        return null;
+      }
+    }
+
+    if (!empty && !value) {
+      error(value, ast, 'Expected non-empty string');
+    }
+
+    if (min != null && value.length < min) {
+      error(value, ast, `Expected minimum length "${min}"`);
+    }
+
+    if (max != null && value.length > max) {
+      error(value, ast, `Expected maximum length "${max}"`);
+    }
+
+    if (pattern != null && !pattern.test(value)) {
+      error(value, ast, 'Unexpected pattern');
+    }
+
+    if (test && !test(value)) {
+      error(value, ast);
+    }
+
+    return value;
   };
 
   return new GraphQLScalarType({
     name: typeName,
     serialize: coerceString,
-    parseValue: coerceString,
+    parseValue,
     parseLiteral(ast) {
-      let {kind, value} = ast;
-      if (kind !== Kind.STRING) {
-        error(value, ast, 'Expected type "String"');
+      const {kind, value} = ast;
+      if (kind === Kind.STRING) {
+        return parseValue(value, ast);
       }
-
-      if (trim) {
-        value = value.trim();
-      } else {
-        if (trimLeft) {
-          value = value.trimLeft();
-        }
-        if (trimRight) {
-          value = value.trimRight();
-        }
-      }
-
-      if (!empty && !value.length) {
-        error(value, ast, 'Expected non-empty string');
-      }
-
-      if (truncate != null) {
-        value = value.substring(0, truncate);
-      }
-
-      if (transform) {
-        value = transform(value);
-      }
-
-      if (min != null && value.length < min) {
-        error(value, ast, `Expected minimum length "${min}"`);
-      }
-
-      if (max != null && value.length > max) {
-        error(value, ast, `Expected maximum length "${max}"`);
-      }
-
-      if (pattern != null && !pattern.test(value)) {
-        error(value, ast, 'Unexpected pattern');
-      }
-
-      if (test && !test(value)) {
-        error(value, ast);
-      }
-
-      return value;
+      return null;
     },
   });
 };
